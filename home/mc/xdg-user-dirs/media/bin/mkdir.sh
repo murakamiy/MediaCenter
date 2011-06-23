822a224469230af035e0d6c38c10585c3e6fec92
Yasumasa Murakami <murakamiy2@gmail.com>
2011-06-24 03:06:20 +0900

    ディレクトリ構成ファイル追加

---
+++ home/mc/xdg-user-dirs/media/bin/mkdir.sh
@@ -0,0 +1,3 @@
+mkdir -p /home/mc/xdg-user-dirs/media/video/{dislike,encode,favorite,removed,resume,thumb,title_encode,title_ts,ts}
+mkdir -p /home/mc/xdg-user-dirs/media/job/{epg,log,state}
+mkdir -p /home/mc/xdg-user-dirs/media/job/state/{01_reserved,02_recording,03_record_finished,04_job_finished,05_encode_reserved,06_encording,07_encorde_finished,adapter,failed}
