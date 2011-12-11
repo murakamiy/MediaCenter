1dfb8f22c370c5b396b4e613fc4e2693c87bf618
Yasumasa Murakami <murakamiy2@gmail.com>
2011-12-12 00:25:10 +0900

    resume 値が取得できない場合の対応を追加

--- home/mc/xdg-user-dirs/media/bin/nautilus/png/mplayer.sh
+++ home/mc/xdg-user-dirs/media/bin/nautilus/png/mplayer.sh
@@ -6,8 +6,10 @@ seek=
 if [ -f $resume ];then
     length=$(grep ANS_LENGTH $resume | tail -n 1 | awk -F '=' '{ printf("%d\n", $2) }')
     percent=$(grep ANS_PERCENT_POSITION $resume | tail -n 1 | awk -F '=' '{ printf("%d\n", $2) }')
-    seek_time=$(($length * $percent / 100))
-    seek=" -s $seek_time "
+    if [ -n "$length" -a -n "$percent" ];then
+        seek_time=$(($length * $percent / 100))
+        seek=" -s $seek_time "
+    fi
 fi
 
 mymplayer -t -r $resume $seek $2
