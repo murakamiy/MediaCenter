c8833af33dee5fb86a8c290acad5d13c1488d295
Yasumasa Murakami <murakamiy2@gmail.com>
2012-01-15 16:39:10 +0900

    buffer size 変更

--- home/mc/xdg-user-dirs/media/kernel/build.sh
+++ home/mc/xdg-user-dirs/media/kernel/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# cat: /dev/dvb/adapter3/dvr0: Value too large for defined data type
+
 src_dir=linux-source-3.0.0
 
 /bin/cp $(ls /boot/config* | sort | tail -n 1) $src_dir/.config
@@ -6,7 +9,7 @@ src_dir=linux-source-3.0.0
 (
 export CONCURRENCY_LEVEL=3
 cd $src_dir
-patch --dry-run -Np1 -i ../dvr_buffer_size.patch  
+patch --dry-run -Np1 -i ../dvr_buffer_size.patch > /dev/null 2>&1
 if [ $? -ne 0 ];then
     echo "ERROR: could not apply patch"
     exit
