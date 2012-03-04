0879003464db0685e9e6d3b8a87baf6c107b622b
Yasumasa Murakami <murakamiy2@gmail.com>
2012-03-04 17:21:32 +0900

    playテーブルのダンプ

--- home/mc/xdg-user-dirs/media/bin/database/init.sh
+++ home/mc/xdg-user-dirs/media/bin/database/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sqlite3 tv.db '.dump play' > sql/dump.sql
 rm -f tv.db
 sqlite3 tv.db '.read sql/create_table.sql'
 for f in $(grep -rl transport-stream-id /home/mc/xdg-user-dirs/media/job/state/04_job_finished/);do
