2b99132cf3dc4e765c384f0ee0e4ec7ae2a8994c
Yasumasa Murakami <murakamiy2@gmail.com>
2012-04-27 06:03:32 +0900

    CSの録画予約廃止

--- home/mc/xdg-user-dirs/media/bin/mkjob.sh
+++ home/mc/xdg-user-dirs/media/bin/mkjob.sh
@@ -7,13 +7,7 @@ log 'starting aggregate'
 python ${MC_DIR_DB_RATING}/aggregate.py >> ${MC_DIR_DB_RATING}/log 2>&1
 
 log 'starting create ts file'
-
-for cc in $(sort -k 4 $MC_FILE_CHANNEL_CS | awk 'BEGIN { prev = 0 } { group = substr($4, 3, 2); if (prev != group) print $1; prev = group }');do
-    rec $cc 60 ${MC_DIR_EPG}/cs_${cc}.ts
-done
-
 rec 101 60 ${MC_DIR_EPG}/bs.ts
-
 for c in $(awk '{ print $1 }' $MC_FILE_CHANNEL_DEGITAL);do
     rec $c 60 ${MC_DIR_EPG}/${c}.ts
 done
@@ -22,10 +16,6 @@ log 'starting epgdump_py'
 for ts in ${MC_DIR_EPG}/[0-9]*.ts;do
     channel=$(basename $ts .ts)
     python $MC_BIN_EPGDUMP -e -c $channel -i $ts -o ${MC_DIR_EPG}/${channel}.xml
-done
-for ts in ${MC_DIR_EPG}/cs_[0-9]*.ts;do
-    channel=$(basename $ts .ts)
-    python $MC_BIN_EPGDUMP -e -d -s -i $ts -o ${MC_DIR_EPG}/${channel}.xml
 done
 python $MC_BIN_EPGDUMP -e -d -b -i ${MC_DIR_EPG}/bs.ts -o ${MC_DIR_EPG}/bs.xml
 
