f2d325205482f041ae4349b74998c8c12473ee54
MediaCenter <mc@MediaCenter>
2013-04-16 22:27:08 +0900

    サンバサーバーのディスクのファイル削除のみに限定する

--- xdg-user-dirs/media/bin/clean.sh
+++ xdg-user-dirs/media/bin/clean.sh
@@ -1,98 +1,17 @@
 #!/bin/bash
 source $(dirname $0)/00.conf
 
-function has_free_space() {
-    used=$(df -Ph --sync | grep '/home$' | awk '{ print $5 }' | tr -d '%' | egrep -o '^[0-9]+$')
-    log "used: $used"
-    if [ -z "$used" ];then
-        return 0
-    fi
-    if [ $used -lt 70 ];then
-        return 0
-    fi
-    return 1
-}
+trash-empty
+find $MC_DIR_RESUME -type f -delete
+find $MC_DIR_MP4 -ctime +28 -delete
+for f in $(smbclient -A ~/.smbauth -D contents -c "ls" $MC_SMB_SERVER |
+    egrep '[[:space:]]A[[:space:]]+[0-9]+[[:space:]]' |
+    awk -F '[[:space:]]A[[:space:]]+[0-9]+[[:space:]]' '
+    {
+        "date +%Y%m%d%H%M%S -d \""$2"\"" | getline time
+        printf("%d\t%s\n", time, $1)
+    }' | sort -k 1 -n -r | sed -n -e '501,$p' | awk '{ print $2 }');do
 
-count=0
-while true;do
-    find ~/.local/share/Trash -type f -delete
-    find $MC_DIR_RESUME -type f -delete
-    find $MC_DIR_MP4 -ctime +28 -delete
-    for f in $(smbclient -A ~/.smbauth -D contents -c "ls" $MC_SMB_SERVER |
-        egrep '[[:space:]]A[[:space:]]+[0-9]+[[:space:]]' |
-        awk -F '[[:space:]]A[[:space:]]+[0-9]+[[:space:]]' '
-        {
-            "date +%Y%m%d%H%M%S -d \""$2"\"" | getline time
-            printf("%d\t%s\n", time, $1)
-        }' | sort -k 1 -n -r | sed -n -e '301,$p' | awk '{ print $2 }');do
-
-        smbclient -A ~/.smbauth -D contents -c "del $f" $MC_SMB_SERVER
-
-    done
-
-    has_free_space
-    if [ $? -eq 0 ];then
-        exit
-    fi
-    if [ $count -gt 10 ];then
-        break
-    fi
-    ((count++))
-
-    for f in $(find $MC_DIR_JOB_FINISHED -type f);do
-        base=$(basename ${f} .xml)
-        if [ ! -f ${MC_DIR_TS}/${base}.ts ];then
-            title=$(xmlsel -t -m '//title' -v '.' $f)
-            log "delete_xml: $f $title"
-            /bin/rm -f $f
-        fi
-    done
-
-    for f in $(find $MC_DIR_ENCODE -type f);do
-        base=$(basename $f | awk -F . '{ print $1 }')
-        thumb_file=$(basename $f)
-        if [ -f "${MC_DIR_THUMB}/${thumb_file}" ];then
-            inode=$(stat --format='%i' ${MC_DIR_THUMB}/${thumb_file})
-            title=$(find $MC_DIR_TITLE_ENCODE -inum $inode)
-            if [ ! -f "$title" ];then
-                log "delete: $base encode"
-                /bin/rm -f $f
-                /bin/rm -f ${MC_DIR_ENCODE_FINISHED}/${base}.xml
-            fi
-        else
-            ln -sf $f /home/mc/NO_TITLE_FILE_${base}
-            ln -sf ${MC_DIR_ENCODE_FINISHED}/${base}.xml /home/mc/NO_TITLE_XML_${base}
-        fi
-    done
-
-    for f in $(find $MC_DIR_TS -type f | sort);do
-        base=$(basename $f | awk -F . '{ print $1 }')
-        thumb_file=$(basename $f)
-        if [ -f "${MC_DIR_THUMB}/${thumb_file}" ];then
-            inode=$(stat --format='%i' ${MC_DIR_THUMB}/${thumb_file})
-            title=$(find $MC_DIR_TITLE_TS -inum $inode)
-            if [ ! -f "$title" ];then
-                title_jp=ts
-                if [ -f "${MC_DIR_JOB_FINISHED}/${base}.xml" ];then
-                    title_jp=$(xmlsel -t -m '//title' -v '.' ${MC_DIR_JOB_FINISHED}/${base}.xml)
-                fi
-                log "delete: $base $title_jp"
-                /bin/rm -f $f
-                /bin/rm -f ${MC_DIR_JOB_FINISHED}/${base}.xml
-            fi
-        else
-            ln -sf $f /home/mc/NO_TITLE_FILE_TS_${base}
-            ln -sf ${MC_DIR_JOB_FINISHED}/${base}.xml /home/mc/NO_TITLE_XML_TS_${base}
-        fi
-
-        has_free_space
-        if [ $? -eq 0 ];then
-            exit
-        fi
-    done
-
-    for f in $(find $MC_DIR_THUMB -type f -links 1);do
-        /bin/rm -f $f
-    done
+    smbclient -A ~/.smbauth -D contents -c "del $f" $MC_SMB_SERVER
 
 done
