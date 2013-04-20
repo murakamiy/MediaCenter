1005b72a282369adf6f71fe0afad9b5fd5cd92d4
MediaCenter <mc@MediaCenter>
2013-04-20 10:13:51 +0900

    USB ハードディスクPOWER ONできない問題の修正

--- xdg-user-dirs/media/bin/mkdir.sh
+++ xdg-user-dirs/media/bin/mkdir.sh
@@ -1,4 +1,4 @@
 mkdir -p /home/mc/xdg-user-dirs/media/video/{encode,encode_hd,removed,thumb,title_encode,title_ts,ts,ts_hd,mp4}
 mkdir -p /home/mc/xdg-user-dirs/media/job/{epg,log,state}
-mkdir -p /home/mc/xdg-user-dirs/media/job/state/{01_reserved,02_recording,03_record_finished,04_job_finished,05_encode_reserved,06_encording,07_encorde_finished,failed}
+mkdir -p /home/mc/xdg-user-dirs/media/job/state/{01_reserved,02_recording,03_record_finished,04_job_finished,05_encode_reserved,06_encording,07_encorde_finished,usb_disk,failed}
 mkdir -p /home/mc/xdg-user-dirs/media/job/state/background/{run,work}
--- xdg-user-dirs/media/bin/place-encode.sh
+++ xdg-user-dirs/media/bin/place-encode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source $(dirname $0)/00.conf
 
-$MC_BIN_USB_POWER_ON
 /usr/bin/nautilus $MC_DIR_TITLE_ENCODE
+$MC_BIN_USB_POWER_ON
