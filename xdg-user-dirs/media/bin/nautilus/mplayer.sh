c8a5de06a4366449fddfa5987ba011356fa52e10
MediaCenter <mc@MediaCenter>
2013-05-04 12:04:59 +0900

    USBハードディスクON ロック時間30分に

--- xdg-user-dirs/media/bin/nautilus/mplayer.sh
+++ xdg-user-dirs/media/bin/nautilus/mplayer.sh
@@ -3,8 +3,8 @@ source $(dirname $0)/../00.conf
 
 (
 touch $MC_DIR_RECORDING/mplayer_$$.xml
-sleep 3600
+sleep 1800
 /bin/rm $MC_DIR_RECORDING/mplayer_$$.xml
 ) &
 
-$MC_BIN_MYMPLAYER -t $2
+exec $MC_BIN_MYMPLAYER -t $2
