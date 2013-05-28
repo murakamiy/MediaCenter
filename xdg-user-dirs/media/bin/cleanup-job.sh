cc3ca07846fcf306be3313cfbbe61aa94d5cbbdf
MediaCenter <mc@MediaCenter>
2013-05-28 20:06:51 +0900

    ビデオ再生時のロックファイルを削除する

--- xdg-user-dirs/media/bin/cleanup-job.sh
+++ xdg-user-dirs/media/bin/cleanup-job.sh
@@ -3,15 +3,6 @@ source $(dirname $0)/00.conf
 
 log "start"
 
-mkjob=false
-if [ -f ${MC_DIR_RECORDING}/mkjob.xml ];then
-    mkjob=true
-fi
-
-find $MC_DIR_RECORDING $MC_DIR_RECORD_FINISHED $MC_DIR_ENCODING -type f -name '*.xml' -delete
-
-if [ $mkjob = "true" ];then
-    bash /home/mc/xdg-user-dirs/media/bin/mkjob.sh
-fi
+find $MC_DIR_RECORDING $MC_DIR_RECORD_FINISHED $MC_DIR_ENCODING $MC_DIR_PLAY -type f -name '*.xml' -delete
 
 log "end"
