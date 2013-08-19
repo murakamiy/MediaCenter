b17e6deb6f15af97f1a89591007681b547931e46
MediaCenter <mc@MediaCenter>
2013-08-19 20:52:40 +0900

    play movie on ssd

--- xdg-user-dirs/media/bin/nautilus/main.sh
+++ xdg-user-dirs/media/bin/nautilus/main.sh
@@ -17,7 +17,7 @@ if [ -n "$thumb_file" ];then
 
     echo $png_file | grep -q $MC_DIR_TITLE_TS
     if [ $? -eq 0 ];then
-        dir=$MC_DIR_TS_HD
+        dir="$MC_DIR_TS_HD $MC_DIR_TS"
     else
         echo $png_file | grep -q $MC_DIR_TITLE_ENCODE
         if [ $? -eq 0 ];then
@@ -35,7 +35,7 @@ if [ -n "$thumb_file" ];then
     fi
     video_file=$(find $dir -type f -name $thumb_file)
 
-    if [ $dir = $MC_DIR_TS_HD ];then
+    if [ "$dir" = "$MC_DIR_TS_HD $MC_DIR_TS" ];then
         trap safe_finish 1 2 3 15
         python ${MC_DIR_DB_RATING}/play.py ${MC_DIR_JOB_FINISHED}/${xml_file} >> ${MC_DIR_DB_RATING}/log 2>&1 &
         pid_play=$!
