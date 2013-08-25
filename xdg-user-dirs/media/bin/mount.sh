ff35d03c99bc31289ed3148c7cc3c8147026eff0
MediaCenter <mc@debian.mediacenter>
2013-08-25 14:43:34 +0900

    usb disk mount

--- xdg-user-dirs/media/bin/mount.sh
+++ xdg-user-dirs/media/bin/mount.sh
@@ -2,7 +2,6 @@
 source $(dirname $0)/00.conf
 log=/tmp/$(basename $0)
 
-
 assemble=false
 if [ -f /proc/mdstat ];then
     grep -q '^md1 : active ' /proc/mdstat
@@ -29,20 +28,20 @@ if [ $? -ne 0 ];then
     mount --bind /mnt/ssd_array/video /home/mc/xdg-user-dirs/media/video
 fi
 
+arr=($($MC_BIN_USB_CONTROL -d))
+echo -e "\nusb disk device : ${arr[@]}\n" >> $log
+
 if [ -e /home/mc/xdg-user-dirs/media/job/state/usb_disk/power_on ];then
     date +"%Y/%m/%d %H:%M:%S.%N power on usb_disk" >> $log
 
-    hdparm -W1 /dev/sdc 
-    hdparm -W1 /dev/sdd 
-    hdparm -W1 /dev/sde 
-    hdparm -W1 /dev/sdf 
+#     hdparm -W1 ${arr[@]}
 
     if [ -e /home/mc/xdg-user-dirs/media/job/state/usb_disk/mount ];then
         date +"%Y/%m/%d %H:%M:%S.%N mount usb_disk" >> $log
 
         mount -o noatime,stripe=5120 /dev/md0p1 /mnt/hd_array
-        mount -o noatime /dev/sde1 /mnt/hd
-        mount -o noatime /dev/sdf1 /mnt/hd2
+        mount -o noatime ${arr[2]}1 /mnt/hd
+        mount -o noatime ${arr[3]}1 /mnt/hd2
         mount --bind /mnt/hd_array/ts_hd /home/mc/xdg-user-dirs/media/video/ts_hd
         mount --bind /mnt/hd/encode_hd   /home/mc/xdg-user-dirs/media/video/encode_hd
     fi
