d0ad9ffbf2919732e5e27df346e5029623c4c386
MediaCenter <mc@debian>
2013-09-19 00:43:34 +0900

    disk usage log

--- xdg-user-dirs/media/bin/safe_shutdown.sh
+++ xdg-user-dirs/media/bin/safe_shutdown.sh
@@ -43,29 +43,30 @@ fi
 next_job_time=$(get_next_job_time)
 wakeup_time=$(python $MC_BIN_WAKEUP_TIME $next_job_time)
 
+log $(df -h | grep ^/dev/sda | awk '{ printf("disk used=%s avail=%s\n", $3, $4) }')
+
 if [ $wakeup_time -ne -1 ];then
     next_wakeup_time=$(awk -v epoc=$wakeup_time 'BEGIN { print strftime("%Y/%m/%d %H:%M:%S", epoc) }')
     log
     echo "next wakeup time: $next_wakeup_time\n\nShutDown ?"
 
     xdpyinfo -display :0.0 > /dev/null
-    avail=$(df -h | grep ^/dev/sda |awk '{ print $4 }')
     if [ $? -eq 0 ];then
         zenity --question --no-wrap --timeout=$timeout --display=:0.0 --text="<span font_desc='40'>next wakeup time: $next_wakeup_time\n\nShutDown ?</span>"
         if [ $? -ne 1 ];then
-            log "shutdown : X Server $avail"
+            log "shutdown=yes : X Server"
             $MC_BIN_USB_POWER_OFF
             sudo $MC_BIN_USB_CONTROL -e
             sudo $MC_BIN_WAKEUPTOOL -w -t $wakeup_time
         fi
     else
-        log "shutdown : console $avail"
+        log "shutdown=yes : console"
         $MC_BIN_USB_POWER_OFF
         sudo $MC_BIN_USB_CONTROL -e
         sudo $MC_BIN_WAKEUPTOOL -w -t $wakeup_time
     fi
 else
-    log
+    log "shutdown=no"
     echo next job will be executed soon
     zenity --warning --no-wrap --timeout=$timeout --display=:0.0 --text="<span font_desc='40'>next job will be executed soon</span>"
 fi
