8d1fd677345ba1ec8bf17e1bcb3cd8f7aa56761c
MediaCenter <mc@debian>
2013-10-26 10:27:56 +0900

    Hard Disk 変更

--- xdg-user-dirs/media/bin/migrate.sh
+++ xdg-user-dirs/media/bin/migrate.sh
@@ -3,10 +3,9 @@ source $(dirname $0)/00.conf
 
 function has_free_space() {
 
-    arr=($($MC_BIN_USB_CONTROL -d))
-    used=$(df -Ph --sync | awk -v dev=${arr[1]}1 '{ if ($1 == dev) printf("%d\n", $5) }')
+    used=$(df -Ph --sync | awk -v dev=$MC_DEVICE_USB_DISK_TS '{ if ($1 == dev) printf("%d\n", $5) }')
     if [ "$1" = "print" ];then
-        log "used : ${arr[1]}1 ${used}%"
+        log "used : $MC_DEVICE_USB_DISK_TS ${used}%"
     fi
     if [ -z "$used" ];then
         return 0
