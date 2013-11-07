a0a68534add2775afe83a8c55a3b6927cf55cdf1
MediaCenter <mc@debian>
2013-11-08 00:10:13 +0900

    rrd Round Robin Database

---
+++ xdg-user-dirs/media/bin/rrd/graph.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+
+rrd_dir=/home/mc/xdg-user-dirs/media/bin/rrd
+png_dir=${rrd_dir}/png
+db_file=${rrd_dir}/stat.rrd
+
+start_date=$(awk 'BEGIN { printf("%s\n", strftime("%Y%m%d\n", systime() - 60 * 60 * 24)) }')
+start_date_str=$(awk 'BEGIN { printf("%s\n", strftime("%Y/%m/%d\n", systime() - 60 * 60 * 24)) }')
+
+LANG=C rrdtool graph ${png_dir}/cpu.png \
+--title "CPU usage $start_date_str" \
+--vertical-label "Percent" \
+--imgformat PNG \
+--start $start_date \
+--end start+24h \
+--x-grid HOUR:1:HOUR:1:HOUR:1:0:%H \
+--upper-limit 100 \
+--width 700 \
+--height 300 \
+DEF:USER=$db_file:CPU_USER:AVERAGE \
+DEF:NICE=$db_file:CPU_NICE:AVERAGE \
+DEF:IOWAIT=$db_file:CPU_IOWAIT:AVERAGE \
+DEF:SYSTEM=$db_file:CPU_SYSTEM:AVERAGE \
+DEF:STEAL=$db_file:CPU_STEAL:AVERAGE \
+DEF:IDLE=$db_file:CPU_IDLE:AVERAGE \
+CDEF:CPU_ALL=USER,NICE,SYSTEM,IOWAIT,STEAL,+,+,+,+ \
+VDEF:IOWAIT_MAX=IOWAIT,MAXIMUM \
+VDEF:USER_MAX=USER,MAXIMUM \
+VDEF:NICE_MAX=NICE,MAXIMUM \
+COMMENT:" " \
+AREA:NICE#00FF00:NICE \
+STACK:USER#FFFF00:USER \
+STACK:IOWAIT#FF0000:IOWAIT \
+COMMENT:" \j" \
+COMMENT:" " \
+STACK:SYSTEM#0000FF:SYSTEM \
+STACK:STEAL#000000:STEAL \
+STACK:IDLE#00FFFF:IDLE \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:CPU_ALL:MIN:"CPU usage minimum\: %2.2lf%%" \
+GPRINT:CPU_ALL:MAX:"CPU usage maximum\: %2.2lf%%" \
+GPRINT:CPU_ALL:AVERAGE:"CPU usage average\: %2.2lf%%" \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:USER_MAX:"USER maximum\: %2.2lf%%" \
+GPRINT:NICE_MAX:"NICE maximum\: %2.2lf%%" \
+GPRINT:IOWAIT_MAX:"IO wait maximum\: %2.2lf%%" \
+COMMENT:" \j"
+
+
+LANG=C rrdtool graph ${png_dir}/io_raid.png \
+--title "IO HD raid $start_date_str" \
+--vertical-label "MB per second" \
+--imgformat PNG \
+--start $start_date \
+--end start+24h \
+--x-grid HOUR:1:HOUR:1:HOUR:1:0:%H \
+--upper-limit 300 \
+--lower-limit -300 \
+--width 700 \
+--height 300 \
+DEF:HD_1_W=$db_file:HD_ARRAY_1_WRITE:AVERAGE \
+DEF:HD_2_W=$db_file:HD_ARRAY_2_WRITE:AVERAGE \
+DEF:HD_3_W=$db_file:HD_ARRAY_3_WRITE:AVERAGE \
+DEF:HD_ALL_W=$db_file:HD_RAID_WRITE:AVERAGE \
+DEF:HD_1_R=$db_file:HD_ARRAY_1_READ:AVERAGE \
+DEF:HD_2_R=$db_file:HD_ARRAY_2_READ:AVERAGE \
+DEF:HD_3_R=$db_file:HD_ARRAY_3_READ:AVERAGE \
+DEF:HD_ALL_R=$db_file:HD_RAID_READ:AVERAGE \
+CDEF:HD_1_R_NEGATIVE=HD_1_R,-1,* \
+CDEF:HD_2_R_NEGATIVE=HD_2_R,-1,* \
+CDEF:HD_3_R_NEGATIVE=HD_3_R,-1,* \
+CDEF:HD_ALL_R_NEGATIVE=HD_ALL_R,-1,* \
+CDEF:HD_1_W_NEGATIVE=HD_1_W,-1,* \
+CDEF:HD_2_W_NEGATIVE=HD_2_W,-1,* \
+CDEF:HD_3_W_NEGATIVE=HD_3_W,-1,* \
+CDEF:HD_ALL_W_NEGATIVE=HD_ALL_W,-1,* \
+VDEF:HD_1_W_MAX=HD_1_W,MAXIMUM \
+VDEF:HD_2_W_MAX=HD_2_W,MAXIMUM \
+VDEF:HD_3_W_MAX=HD_3_W,MAXIMUM \
+VDEF:HD_ALL_W_MAX=HD_ALL_W,MAXIMUM \
+VDEF:HD_1_R_MAX=HD_1_R,MAXIMUM \
+VDEF:HD_2_R_MAX=HD_2_R,MAXIMUM \
+VDEF:HD_3_R_MAX=HD_3_R,MAXIMUM \
+VDEF:HD_ALL_R_MAX=HD_ALL_R,MAXIMUM \
+VDEF:HD_1_W_TOTAL=HD_1_W,TOTAL \
+VDEF:HD_2_W_TOTAL=HD_2_W,TOTAL \
+VDEF:HD_3_W_TOTAL=HD_3_W,TOTAL \
+VDEF:HD_ALL_W_TOTAL=HD_ALL_W,TOTAL \
+VDEF:HD_1_R_TOTAL=HD_1_R,TOTAL \
+VDEF:HD_2_R_TOTAL=HD_2_R,TOTAL \
+VDEF:HD_3_R_TOTAL=HD_3_R,TOTAL \
+VDEF:HD_ALL_R_TOTAL=HD_ALL_R,TOTAL \
+COMMENT:" " \
+LINE1:HD_ALL_W#000000:"raid write" \
+LINE1:HD_ALL_R#000000:"raid read" \
+COMMENT:" \j" \
+COMMENT:" " \
+AREA:HD_1_W#FF8C00:"HD1 write"  \
+STACK:HD_2_W#FFFF00:"HD2 write" \
+STACK:HD_3_W#FF0000:"HD3 write" \
+COMMENT:" \j" \
+COMMENT:" " \
+AREA:HD_1_R_NEGATIVE#0000FF:"HD1 read"  \
+STACK:HD_2_R_NEGATIVE#00FF00:"HD2 read"  \
+STACK:HD_3_R_NEGATIVE#9400D3:"HD3 read" \
+COMMENT:" \j" \
+COMMENT:" write maximum MB/s" \
+GPRINT:HD_ALL_W_MAX:"raid \: %3.2lf" \
+GPRINT:HD_1_W_MAX:"HD1 \: %3.2lf" \
+GPRINT:HD_2_W_MAX:"HD2 \: %3.2lf" \
+GPRINT:HD_3_W_MAX:"HD3 \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" read  maximum MB/s" \
+GPRINT:HD_ALL_R_MAX:"raid \: %3.2lf" \
+GPRINT:HD_1_R_MAX:"HD1 \: %3.2lf" \
+GPRINT:HD_2_R_MAX:"HD2 \: %3.2lf" \
+GPRINT:HD_3_R_MAX:"HD3 \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" write total MB/s" \
+GPRINT:HD_ALL_W_TOTAL:"raid \: %3.2lf" \
+GPRINT:HD_1_W_TOTAL:"HD1 \: %3.2lf" \
+GPRINT:HD_2_W_TOTAL:"HD2 \: %3.2lf" \
+GPRINT:HD_3_W_TOTAL:"HD3 \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" read  total MB/s" \
+GPRINT:HD_ALL_R_TOTAL:"raid \: %3.2lf" \
+GPRINT:HD_1_R_TOTAL:"HD1 \: %3.2lf" \
+GPRINT:HD_2_R_TOTAL:"HD2 \: %3.2lf" \
+GPRINT:HD_3_R_TOTAL:"HD3 \: %3.2lf" \
+COMMENT:" \j"
+
+
+LANG=C rrdtool graph ${png_dir}/io.png \
+--title "IO SSD HD $start_date_str" \
+--vertical-label "MB per second" \
+--imgformat PNG \
+--start $start_date \
+--end start+24h \
+--x-grid HOUR:1:HOUR:1:HOUR:1:0:%H \
+--upper-limit 300 \
+--lower-limit -300 \
+--width 700 \
+--height 300 \
+DEF:SSD_R=$db_file:SSD_READ:AVERAGE \
+DEF:HD_R=$db_file:HD_READ:AVERAGE \
+DEF:SSD_W=$db_file:SSD_WRITE:AVERAGE \
+DEF:HD_W=$db_file:HD_WRITE:AVERAGE \
+CDEF:SSD_R_NEGATIVE=SSD_R,-1,* \
+CDEF:HD_R_NEGATIVE=HD_R,-1,* \
+VDEF:SSD_R_MAX=SSD_R,MAXIMUM \
+VDEF:HD_R_MAX=HD_R,MAXIMUM \
+VDEF:SSD_W_MAX=SSD_W,MAXIMUM \
+VDEF:HD_W_MAX=HD_W,MAXIMUM \
+VDEF:SSD_R_TOTAL=SSD_R,TOTAL \
+VDEF:HD_R_TOTAL=HD_R,TOTAL \
+VDEF:SSD_W_TOTAL=SSD_W,TOTAL \
+VDEF:HD_W_TOTAL=HD_W,TOTAL \
+COMMENT:" " \
+AREA:SSD_W#FF8C00:"SSD write" \
+STACK:HD_W#FFFF00:"HD write" \
+COMMENT:" \j" \
+COMMENT:" " \
+AREA:SSD_R_NEGATIVE#0000FF:"SSD read" \
+STACK:HD_R_NEGATIVE#00FF00:"HD read" \
+COMMENT:" \j" \
+COMMENT:" " \
+COMMENT:" write maximum MB/s" \
+GPRINT:SSD_W_MAX:"SSD \: %3.2lf" \
+GPRINT:HD_W_MAX:"HD \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" " \
+COMMENT:" read  maximum MB/s" \
+GPRINT:SSD_R_MAX:"SSD \: %3.2lf" \
+GPRINT:HD_R_MAX:"HD \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" " \
+COMMENT:" write total MB/s" \
+GPRINT:SSD_W_TOTAL:"SSD \: %3.2lf" \
+GPRINT:HD_W_TOTAL:"HD \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" " \
+COMMENT:" read  total MB/s" \
+GPRINT:SSD_R_TOTAL:"SSD \: %3.2lf" \
+GPRINT:HD_R_TOTAL:"HD \: %3.2lf" \
+COMMENT:" \j"
+
+
+LANG=C rrdtool graph ${png_dir}/temp.png \
+--title "Temparature $start_date_str" \
+--imgformat PNG \
+--start $start_date \
+--end start+24h \
+--x-grid HOUR:1:HOUR:1:HOUR:1:0:%H \
+--upper-limit 100 \
+--lower-limit -100 \
+--width 700 \
+--height 300 \
+DEF:LOAD_AVERAGE=$db_file:LOAD_AVERAGE:AVERAGE \
+DEF:TEMP_CPU=$db_file:TEMP_CPU:AVERAGE \
+DEF:TEMP_MOTHER_BORD_1=$db_file:TEMP_MOTHER_BORD_1:AVERAGE \
+DEF:TEMP_MOTHER_BORD_2=$db_file:TEMP_MOTHER_BORD_2:AVERAGE \
+DEF:VOLT_IN0=$db_file:VOLT_IN0:AVERAGE \
+DEF:VOLT_IN1=$db_file:VOLT_IN1:AVERAGE \
+DEF:VOLT_IN2=$db_file:VOLT_IN2:AVERAGE \
+DEF:VOLT_IN3=$db_file:VOLT_IN3:AVERAGE \
+DEF:VOLT_IN4=$db_file:VOLT_IN4:AVERAGE \
+DEF:VOLT_IN5=$db_file:VOLT_IN5:AVERAGE \
+DEF:VOLT_IN6=$db_file:VOLT_IN6:AVERAGE \
+DEF:VOLT_3VSB=$db_file:VOLT_3VSB:AVERAGE \
+DEF:VOLT_VBAT=$db_file:VOLT_VBAT:AVERAGE \
+DEF:FAN1=$db_file:FAN1:AVERAGE \
+DEF:FAN2=$db_file:FAN2:AVERAGE \
+CDEF:LOAD_AVERAGE_PERCENT=LOAD_AVERAGE,25,* \
+CDEF:TEMP_CPU_PERCENT=TEMP_CPU \
+CDEF:TEMP_MOTHER_BORD_1_PERCENT=TEMP_MOTHER_BORD_1 \
+CDEF:TEMP_MOTHER_BORD_2_PERCENT=TEMP_MOTHER_BORD_2 \
+CDEF:FAN1_PERCENT=FAN1,100,1400,-1,*,/,* \
+CDEF:FAN2_PERCENT=FAN2,100,1500,-1,*,/,* \
+CDEF:VOLT_IN0_PERCENT=VOLT_IN0,100,27.54,-1,*,/,* \
+CDEF:VOLT_IN1_PERCENT=VOLT_IN1,100,27.54,-1,*,/,* \
+CDEF:VOLT_IN2_PERCENT=VOLT_IN2,100,27.54,-1,*,/,* \
+CDEF:VOLT_IN3_PERCENT=VOLT_IN3,100,27.54,-1,*,/,* \
+CDEF:VOLT_IN4_PERCENT=VOLT_IN4,100,27.54,-1,*,/,* \
+CDEF:VOLT_IN5_PERCENT=VOLT_IN5,100,27.54,-1,*,/,* \
+CDEF:VOLT_IN6_PERCENT=VOLT_IN6,100,27.54,-1,*,/,* \
+CDEF:VOLT_3VSB_PERCENT=VOLT_3VSB,100,27.54,-1,*,/,* \
+VDEF:LOAD_AVERAGE_MAX=LOAD_AVERAGE,MAXIMUM \
+VDEF:TEMP_CPU_MAX=TEMP_CPU,MAXIMUM \
+VDEF:VOLT_IN0_MAX=VOLT_IN0,MAXIMUM \
+VDEF:VOLT_IN1_MAX=VOLT_IN1,MAXIMUM \
+VDEF:VOLT_IN2_MAX=VOLT_IN2,MAXIMUM \
+VDEF:VOLT_IN3_MAX=VOLT_IN3,MAXIMUM \
+VDEF:VOLT_IN4_MAX=VOLT_IN4,MAXIMUM \
+VDEF:VOLT_IN5_MAX=VOLT_IN5,MAXIMUM \
+VDEF:VOLT_IN6_MAX=VOLT_IN6,MAXIMUM \
+VDEF:VOLT_3VSB_MAX=VOLT_3VSB,MAXIMUM \
+VDEF:FAN1_MAX=FAN1,MAXIMUM \
+VDEF:FAN2_MAX=FAN2,MAXIMUM \
+COMMENT:" " \
+AREA:TEMP_CPU_PERCENT#FF8C00:"cpu temp" \
+LINE1:LOAD_AVERAGE_PERCENT#000000:"load average" \
+COMMENT:" \j" \
+COMMENT:" " \
+AREA:VOLT_IN0_PERCENT#0000FF:"volt0" \
+STACK:VOLT_IN1_PERCENT#8A2BE2:"volt1" \
+STACK:VOLT_IN2_PERCENT#7CFC00:"volt2" \
+STACK:VOLT_IN3_PERCENT#A52A2A:"volt3" \
+STACK:VOLT_IN4_PERCENT#0000CD:"volt4" \
+STACK:VOLT_IN5_PERCENT#FF00FF:"volt5" \
+STACK:VOLT_IN6_PERCENT#228B22:"volt6" \
+STACK:VOLT_3VSB_PERCENT#00BFFF:"volt 3vsb" \
+COMMENT:" \j" \
+COMMENT:" " \
+LINE1:FAN1_PERCENT#000000:"fan1" \
+LINE1:FAN2_PERCENT#FF0000:"fan2" \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:TEMP_CPU_MAX:"cpu temparature max \: %3.2lf C" \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:LOAD_AVERAGE_MAX:"load average max \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" " \
+COMMENT:" VOLT MAX" \
+GPRINT:VOLT_IN1_MAX:"1 \: %3.2lf" \
+GPRINT:VOLT_IN2_MAX:"2 \: %3.2lf" \
+GPRINT:VOLT_IN3_MAX:"3 \: %3.2lf" \
+GPRINT:VOLT_IN4_MAX:"4 \: %3.2lf" \
+GPRINT:VOLT_IN5_MAX:"5 \: %3.2lf" \
+GPRINT:VOLT_IN6_MAX:"6 \: %3.2lf" \
+GPRINT:VOLT_3VSB_MAX:"3VSB \: %3.2lf" \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:FAN1_MAX:"fan1 \: %4.0lf RPM" \
+GPRINT:FAN2_MAX:"fan2 \: %4.0lf RPM" \
+COMMENT:" \j"
+
+
+LANG=C rrdtool graph ${png_dir}/mem.png \
+--title "Memory usage $start_date_str" \
+--imgformat PNG \
+--start $start_date \
+--end start+24h \
+--x-grid HOUR:1:HOUR:1:HOUR:1:0:%H \
+--upper-limit 8000 \
+--width 700 \
+--height 300 \
+DEF:MEMORY=$db_file:MEMORY:AVERAGE \
+VDEF:MEMORY_MAX=MEMORY,MAXIMUM \
+COMMENT:" " \
+AREA:MEMORY#FF8C00:"memory" \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:MEMORY_MAX:"memory max \: %4.0lf" \
+COMMENT:" \j"
+
+
+
+
+LANG=C rrdtool graph ${png_dir}/du.png \
+--title "Disk usage $start_date_str" \
+--imgformat PNG \
+--start $start_date \
+--end start+24h \
+--x-grid HOUR:1:HOUR:1:HOUR:1:0:%H \
+--upper-limit 100  \
+--width 700 \
+--height 300 \
+DEF:DISK_USAGE=$db_file:DISK_USAGE:AVERAGE \
+VDEF:DISK_USAGE_MAX=DISK_USAGE,MAXIMUM \
+COMMENT:" " \
+AREA:DISK_USAGE#FF8C00:"disk usage" \
+COMMENT:" \j" \
+COMMENT:" " \
+GPRINT:DISK_USAGE_MAX:"disk usage max \: %3.0lf" \
+COMMENT:" \j"
---
+++ xdg-user-dirs/media/bin/rrd/update.sh
@@ -0,0 +1,321 @@
+#!/bin/bash
+
+db_file=/home/mc/xdg-user-dirs/media/bin/rrd/stat.rrd
+
+iostat_arr=($(LANG=C iostat -ym -j ID 60 1 |
+awk '
+BEGIN {
+    CPU_USER = 0
+    CPU_NICE = 0
+    CPU_SYSTEM = 0
+    CPU_IOWAIT = 0
+    CPU_STEAL = 0
+    CPU_IDLE = 0
+    SSD_READ = 0
+    SSD_WRITE = 0
+    HD_ARRAY_1_READ = 0
+    HD_ARRAY_1_WRITE = 0
+    HD_ARRAY_2_READ = 0
+    HD_ARRAY_2_WRITE = 0
+    HD_ARRAY_3_READ = 0
+    HD_ARRAY_3_WRITE = 0
+    HD_RAID_READ = 0
+    HD_RAID_WRITE = 0
+    HD_READ = 0
+    HD_WRITE = 0
+
+    cpu_line = 0
+    ssd_line = 0
+    hd_array_1_line = 0
+    hd_array_2_line = 0
+    hd_array_3_line = 0
+    hd_line = 0
+    hd_raid_line = 0
+}
+
+/^avg-cpu: / {
+    cpu_line = 1
+    next
+}
+/^ata-TOSHIBA_THNSNH256GCST_73IS101WTPHY/ {
+    ssd_line = 1
+    next
+}
+/^ata-WDC_WD30EZRX-00D8PB0_WD-WMC4N0640397/ {
+    hd_array_1_line = 1
+    next
+}
+/^ata-WDC_WD30EZRX-00D8PB0_WD-WMC4N0637164/ {
+    hd_array_2_line = 1
+    next
+}
+/^ata-WDC_WD30EZRX-00D8PB0_WD-WMC4N0680500/ {
+    hd_array_3_line = 1
+    next
+}
+/^ata-WDC_WD25EZRX-00MMMB0_WD-WCAWZ1234078/ {
+    hd_line = 1
+    next
+}
+/^md-name-debian:0/ {
+    hd_raid_line = 1
+    next
+}
+
+{
+    if (cpu_line == 1) {
+        cpu_line = 0
+        CPU_USER = $1
+        CPU_NICE = $2
+        CPU_SYSTEM = $3
+        CPU_IOWAIT = $4
+        CPU_STEAL = $5
+        CPU_IDLE = $6
+    }
+    else if (ssd_line == 1) {
+        ssd_line = 0
+        SSD_READ = $2
+        SSD_WRITE = $3
+    }
+    else if (hd_array_1_line == 1) {
+        hd_array_1_line = 0
+        HD_ARRAY_1_READ = $2
+        HD_ARRAY_1_WRITE = $3
+    }
+    else if (hd_array_2_line == 1) {
+        hd_array_2_line = 0
+        HD_ARRAY_2_READ = $2
+        HD_ARRAY_2_WRITE = $3
+    }
+    else if (hd_array_3_line == 1) {
+        hd_array_3_line = 0
+        HD_ARRAY_3_READ = $2
+        HD_ARRAY_3_WRITE = $3
+    }
+    else if (hd_line == 1) {
+        hd_line = 0
+        HD_READ = $2
+        HD_WRITE = $3
+    }
+    else if (hd_raid_line == 1) {
+        hd_raid_line = 0
+        HD_RAID_READ = $2
+        HD_RAID_WRITE = $3
+    }
+}
+
+END {
+
+    printf("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s \n",
+            CPU_USER,
+            CPU_NICE,
+            CPU_SYSTEM,
+            CPU_IOWAIT,
+            CPU_STEAL,
+            CPU_IDLE,
+            SSD_READ,
+            SSD_WRITE,
+            HD_ARRAY_1_READ,
+            HD_ARRAY_1_WRITE,
+            HD_ARRAY_2_READ,
+            HD_ARRAY_2_WRITE,
+            HD_ARRAY_3_READ,
+            HD_ARRAY_3_WRITE,
+            HD_RAID_READ,
+            HD_RAID_WRITE,
+            HD_READ,
+            HD_WRITE)
+}'))
+
+CPU_USER=${iostat_arr[0]}
+CPU_NICE=${iostat_arr[1]}
+CPU_SYSTEM=${iostat_arr[2]}
+CPU_IOWAIT=${iostat_arr[3]}
+CPU_STEAL=${iostat_arr[4]}
+CPU_IDLE=${iostat_arr[5]}
+SSD_READ=${iostat_arr[6]}
+SSD_WRITE=${iostat_arr[7]}
+HD_ARRAY_1_READ=${iostat_arr[8]}
+HD_ARRAY_1_WRITE=${iostat_arr[9]}
+HD_ARRAY_2_READ=${iostat_arr[10]}
+HD_ARRAY_2_WRITE=${iostat_arr[11]}
+HD_ARRAY_3_READ=${iostat_arr[12]}
+HD_ARRAY_3_WRITE=${iostat_arr[13]}
+HD_RAID_READ=${iostat_arr[14]}
+HD_RAID_WRITE=${iostat_arr[15]}
+HD_READ=${iostat_arr[16]}
+HD_WRITE=${iostat_arr[17]}
+
+LOAD_AVERAGE=$(uptime | awk -F 'load average: ' '{ print $2 }' | awk -F , '{ print $1 }')
+MEMORY=$(free -m | grep '^-/+ buffers/cache:' | awk '{ print $3 }')
+DISK_USAGE=$(LANG=C df -P | grep '/$' | awk '{ printf("%d\n", $(NF - 1)) }')
+
+sensors_arr=($(LANG=C sensors -A | awk '
+BEGIN {
+
+    TEMP_CPU = 0
+    TEMP_MOTHER_BORD_1 = 0
+    TEMP_MOTHER_BORD_2 = 0
+    VOLT_IN0 = 0
+    VOLT_IN1 = 0
+    VOLT_IN2 = 0
+    VOLT_IN3 = 0
+    VOLT_IN4 = 0
+    VOLT_IN5 = 0
+    VOLT_IN6 = 0
+    VOLT_3VSB = 0
+    VOLT_VBAT = 0
+    FAN1 = 0
+    FAN2 = 0
+
+    temp_mother_board_line = 0
+    temp_cpu_line = 0
+    volt_fan_line = 0
+}
+
+/^acpitz-virtual-0/ {
+    temp_mother_board_line = 1
+    next
+}
+
+/^coretemp-isa-0000/ {
+    temp_cpu_line = 1
+    next
+}
+
+/^it8771-isa-0290/ {
+    volt_fan_line = 1
+    next
+}
+
+
+{
+
+    if (temp_mother_board_line == 1) {
+        if (match($1, "temp1:") != 0) {
+            TEMP_MOTHER_BORD_1 = $2
+        }
+        else if (match($1, "temp2:") != 0) {
+            temp_mother_board_line = 0
+            TEMP_MOTHER_BORD_2 = $2
+        }
+    }
+    else if (temp_cpu_line == 1) {
+        if (match($0, "^Physical id 0:") != 0) {
+            temp_cpu_line = 0
+            TEMP_CPU = $4
+        }
+    }
+    else if (volt_fan_line == 1) {
+        if (match($1, "in0:") != 0) {
+            VOLT_IN0 = $2
+        }
+        else if (match($1, "in1:") != 0) {
+            VOLT_IN1 = $2
+        }
+        else if (match($1, "in2:") != 0) {
+            VOLT_IN2 = $2
+        }
+        else if (match($1, "in3:") != 0) {
+            VOLT_IN3 = $2
+        }
+        else if (match($1, "in4:") != 0) {
+            VOLT_IN4 = $2
+        }
+        else if (match($1, "in5:") != 0) {
+            VOLT_IN5 = $2
+        }
+        else if (match($1, "in6:") != 0) {
+            VOLT_IN6 = $2
+        }
+        else if (match($1, "3VSB:") != 0) {
+            VOLT_3VSB = $2
+        }
+        else if (match($1, "Vbat:") != 0) {
+            VOLT_VBAT = $2
+        }
+        else if (match($1, "fan1:") != 0) {
+            FAN1 = $2
+        }
+        else if (match($1, "fan2:") != 0) {
+            volt_fan_line = 0
+            FAN2 = $2
+        }
+    }
+
+}
+
+END {
+
+    printf("%.1f %.1f %.1f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %d %d\n",
+            TEMP_CPU,
+            TEMP_MOTHER_BORD_1,
+            TEMP_MOTHER_BORD_2,
+            VOLT_IN0,
+            VOLT_IN1,
+            VOLT_IN2,
+            VOLT_IN3,
+            VOLT_IN4,
+            VOLT_IN5,
+            VOLT_IN6,
+            VOLT_3VSB,
+            VOLT_VBAT,
+            FAN1,
+            FAN2)
+}'))
+
+TEMP_CPU=${sensors_arr[0]}
+TEMP_MOTHER_BORD_1=${sensors_arr[1]}
+TEMP_MOTHER_BORD_2=${sensors_arr[2]}
+VOLT_IN0=${sensors_arr[3]}
+VOLT_IN1=${sensors_arr[4]}
+VOLT_IN2=${sensors_arr[5]}
+VOLT_IN3=${sensors_arr[6]}
+VOLT_IN4=${sensors_arr[7]}
+VOLT_IN5=${sensors_arr[8]}
+VOLT_IN6=${sensors_arr[9]}
+VOLT_3VSB=${sensors_arr[10]}
+VOLT_VBAT=${sensors_arr[11]}
+FAN1=${sensors_arr[12]}
+FAN2=${sensors_arr[13]}
+
+# cat << EOF
+# CPU_USER            $CPU_USER
+# CPU_NICE            $CPU_NICE
+# CPU_SYSTEM          $CPU_SYSTEM
+# CPU_IOWAIT          $CPU_IOWAIT
+# CPU_STEAL           $CPU_STEAL
+# CPU_IDLE            $CPU_IDLE
+# SSD_READ            $SSD_READ
+# SSD_WRITE           $SSD_WRITE
+# HD_ARRAY_1_READ     $HD_ARRAY_1_READ
+# HD_ARRAY_1_WRITE    $HD_ARRAY_1_WRITE
+# HD_ARRAY_2_READ     $HD_ARRAY_2_READ
+# HD_ARRAY_2_WRITE    $HD_ARRAY_2_WRITE
+# HD_ARRAY_3_READ     $HD_ARRAY_3_READ
+# HD_ARRAY_3_WRITE    $HD_ARRAY_3_WRITE
+# HD_RAID_READ        $HD_RAID_READ
+# HD_RAID_WRITE       $HD_RAID_WRITE
+# HD_READ             $HD_READ
+# HD_WRITE            $HD_WRITE
+# LOAD_AVERAGE        $LOAD_AVERAGE
+# MEMORY              $MEMORY
+# DISK_USAGE          $DISK_USAGE
+# TEMP_CPU            $TEMP_CPU
+# TEMP_MOTHER_BORD_1  $TEMP_MOTHER_BORD_1
+# TEMP_MOTHER_BORD_2  $TEMP_MOTHER_BORD_2
+# VOLT_IN0            $VOLT_IN0
+# VOLT_IN1            $VOLT_IN1
+# VOLT_IN2            $VOLT_IN2
+# VOLT_IN3            $VOLT_IN3
+# VOLT_IN4            $VOLT_IN4
+# VOLT_IN5            $VOLT_IN5
+# VOLT_IN6            $VOLT_IN6
+# VOLT_3VSB           $VOLT_3VSB
+# VOLT_VBAT           $VOLT_VBAT
+# FAN1                $FAN1
+# FAN2                $FAN2
+# EOF
+
+rrdtool update $db_file \
+N:$CPU_USER:$CPU_NICE:$CPU_SYSTEM:$CPU_IOWAIT:$CPU_STEAL:$CPU_IDLE:$SSD_READ:$SSD_WRITE:$HD_ARRAY_1_READ:$HD_ARRAY_1_WRITE:$HD_ARRAY_2_READ:$HD_ARRAY_2_WRITE:$HD_ARRAY_3_READ:$HD_ARRAY_3_WRITE:$HD_RAID_READ:$HD_RAID_WRITE:$HD_READ:$HD_WRITE:$LOAD_AVERAGE:$MEMORY:$DISK_USAGE:$TEMP_CPU:$TEMP_MOTHER_BORD_1:$TEMP_MOTHER_BORD_2:$VOLT_IN0:$VOLT_IN1:$VOLT_IN2:$VOLT_IN3:$VOLT_IN4:$VOLT_IN5:$VOLT_IN6:$VOLT_3VSB:$VOLT_VBAT:$FAN1:$FAN2
