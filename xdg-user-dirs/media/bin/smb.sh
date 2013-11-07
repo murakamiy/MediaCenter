4a00ba2de43c73bcf7b08c209c29f28a21420f78
MediaCenter <mc@debian>
2013-11-08 00:05:12 +0900

    smb delete

--- xdg-user-dirs/media/bin/smb.sh
+++ xdg-user-dirs/media/bin/smb.sh
@@ -4,6 +4,8 @@ source $(dirname $0)/00.conf
 avail=$(smbclient -A ~/.smbauth -c ls $MC_SMB_SERVER | tail -n 1 | awk -F . '{ print $2 }')
 log "smb migrate start $avail"
 
+total_size=0
+total_count=0
 for f in $(smbclient -A ~/.smbauth -D contents -c "ls" $MC_SMB_SERVER |
     egrep '[[:space:]]A[[:space:]]+[0-9]+[[:space:]]' |
     awk -F '[[:space:]]A[[:space:]]+[0-9]+[[:space:]]' '
@@ -12,9 +14,16 @@ for f in $(smbclient -A ~/.smbauth -D contents -c "ls" $MC_SMB_SERVER |
         printf("%d\t%s\n", time, $1)
     }' | sort -k 1 -n -r | sed -n -e '301,$p' | awk '{ print $2 }');do
 
+    size=$(smbclient -A ~/.smbauth -D contents -c "ls \"$f\"" $MC_SMB_SERVER |
+    head -n 1 | awk -F ' A ' '{ print $2 }' | awk '{ print $1}')
+    total_size=$(($total_size + $size))
+    total_count=$(($total_count + 1))
+
     smbclient -A ~/.smbauth -D contents -c "del \"$f\"" $MC_SMB_SERVER
 
 done
+
+log "smb delete $total_count files $(($total_size / 1024 / 1024))MB"
 
 cd $MC_DIR_TMP
 for f in $(cd $MC_DIR_MP4; find . -name '*.mp4' -size +10M -printf '%f\n');do
