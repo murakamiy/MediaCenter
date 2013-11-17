832dfbb2c2704e475d9269b0361e166fd544d3fd
MediaCenter <mc@debian>
2013-11-17 13:27:13 +0900

    reserve log

--- xdg-user-dirs/media/bin/reserver/reserve.py
+++ xdg-user-dirs/media/bin/reserver/reserve.py
@@ -133,9 +133,9 @@ class ReserveMaker:
                 job_list = job_list[0:2]
         self.log("removed: ")
         for r in remove_list:
-            self.log(" %s %3d %s %.2f %s" % (r.pinfo.start, r.pinfo.rectime / 60, r.pinfo.channel, r.pinfo.priority, r.pinfo.title))
+            self.log(" %s %3d %s %6.2f %s" % (r.pinfo.start, r.pinfo.rectime / 60, r.pinfo.channel, r.pinfo.priority, r.pinfo.title))
             fd = open(DIR_REMOVED + '/' + re.sub('[\'"#$%&()!/*=~<>]', '_', r.pinfo.title) + '.txt', "w")
-            print >> fd, "%s %s %3d %s %.2f %s" % (time.strftime("%H:%M:%S"), r.pinfo.start, r.pinfo.rectime / 60, r.pinfo.channel, r.pinfo.priority, r.pinfo.title)
+            print >> fd, "%s %s %3d %s %6.2f %s" % (time.strftime("%H:%M:%S"), r.pinfo.start, r.pinfo.rectime / 60, r.pinfo.channel, r.pinfo.priority, r.pinfo.title)
             fd.close()
             try:
                 rinfo_list.remove(r)
@@ -193,7 +193,7 @@ class ReserveMaker:
             fd = open(r.pinfo.file_reserved, "w")
             ElementTree(r.element).write(fd, 'utf-8')
             fd.close()
-            self.log(" %s %3d %s %.2f %s" % (r.pinfo.start, r.pinfo.rectime / 60, r.pinfo.channel, r.pinfo.priority, r.pinfo.title))
+            self.log(" %s %3d %s %6.2f %s" % (r.pinfo.start, r.pinfo.rectime / 60, r.pinfo.channel, r.pinfo.priority, r.pinfo.title))
     def parse_xml(self, xml_file):
         tree = ElementTree()
         try:
