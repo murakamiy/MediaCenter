eb056d5c8f407a031147cd9560d5679efabbad93
MediaCenter <mc@debian>
2014-04-23 22:14:56 +0900

    create channel

--- xdg-user-dirs/media/bin/channel/digital/01_rec_digital.sh
+++ xdg-user-dirs/media/bin/channel/digital/01_rec_digital.sh
@@ -4,8 +4,18 @@ for ((i=13; i<=62; i++));do
 
         echo "#---->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#"       >> log.txt
         echo "# CHANNEL=${i}"                                                          >> log.txt
-        echo recpt1 --b25 --strip --sid hd ${i} 120 ${i}.ts                            >> log.txt
-        recpt1 --b25 --strip --sid hd ${i} 120 ${i}.ts                                 >> log.txt 2>&1
+        echo recpt1 --b25 ${i} 60 ${i}.ts                                              >> log.txt
+        recpt1 --b25 ${i} 60 ${i}.ts                                                   >> log.txt 2>&1
+        echo "#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<----#"       >> log.txt
+
+done
+
+for ((i=13; i<=63; i++));do
+
+        echo "#---->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#"       >> log.txt
+        echo "# CHANNEL=C${i}"                                                         >> log.txt
+        echo recpt1 --b25 C${i} 60 C${i}.ts                                            >> log.txt
+        recpt1 --b25 C${i} 60 C${i}.ts                                                 >> log.txt 2>&1
         echo "#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<----#"       >> log.txt
 
 done
