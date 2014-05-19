552b9bc8ae2a4c030417b8bf79ea051cc0259f3b
MediaCenter <mc@debian>
2014-05-19 12:32:44 +0900

    channel setting

--- xdg-user-dirs/media/bin/channel/bs_cs/01_rec_bs_all.sh
+++ xdg-user-dirs/media/bin/channel/bs_cs/01_rec_bs_all.sh
@@ -6,8 +6,8 @@ for node in $(seq -w 1 23);do
 
         echo "#---->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#"       >> log.txt
         echo "# CHANNEL=BS${node}_${slot}"                                             >> log.txt
-        echo recpt1 --b25 --strip --sid hd BS${node}_${slot} 120 BS${node}_${slot}.ts  >> log.txt
-        recpt1 --b25 --strip --sid hd BS${node}_${slot} 120 BS${node}_${slot}.ts       >> log.txt 2>&1
+        echo "recpt1 --b25 --sid hd BS${node}_${slot} 60 BS${node}_${slot}.ts"         >> log.txt
+        recpt1 --b25 --sid hd BS${node}_${slot} 60 BS${node}_${slot}.ts                >> log.txt 2>&1
         echo "#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<----#"       >> log.txt
 
     done
--- xdg-user-dirs/media/bin/channel/bs_cs/01_rec_cs_all.sh
+++ xdg-user-dirs/media/bin/channel/bs_cs/01_rec_cs_all.sh
@@ -4,8 +4,8 @@ for ((i=2; i<=24; i++));do
 
         echo "#---->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#"       >> log.txt
         echo "# CHANNEL=CS${i}"                                                        >> log.txt
-        echo recpt1 --b25 --strip --sid hd CS${i} 120 CS${i}.ts                        >> log.txt
-        recpt1 --b25 --strip --sid hd CS${i} 120 CS${i}.ts                             >> log.txt 2>&1
+        echo "recpt1 --b25 --sid hd CS${i} 60 CS${i}.ts"                               >> log.txt
+        recpt1 --b25 --sid hd CS${i} 60 CS${i}.ts                                      >> log.txt 2>&1
         echo "#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<----#"       >> log.txt
 
 done
