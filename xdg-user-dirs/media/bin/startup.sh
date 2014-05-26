8808202debcdca05607779a8552b49970cd1c932
MediaCenter <mc@debian>
2014-05-27 05:00:25 +0900

    playstation game controller

--- xdg-user-dirs/media/bin/startup.sh
+++ xdg-user-dirs/media/bin/startup.sh
@@ -9,5 +9,6 @@ find $MC_DIR_RECORDING $MC_DIR_RECORD_FINISHED $MC_DIR_ENCODING $MC_DIR_PLAY -ty
 /bin/rm $MC_STAT_MIGRATE
 $MC_BIN_USB_CONTROL -w
 $MC_BIN_USB_POWER_OFF >> ${MC_DIR_LOG}/usb-disk.log 2>&1
+sudo /etc/init.d/sixad restart
 
 log "end"
