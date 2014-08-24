3ee21e0cf56abc2fa212847ce0f17c4109d7253d
MediaCenter <mc@debian>
2014-08-24 22:09:22 +0900

    atd job scheduler delays 15 seconds

--- xdg-user-dirs/media/bin/startup.sh
+++ xdg-user-dirs/media/bin/startup.sh
@@ -3,6 +3,7 @@ source $(dirname $0)/00.conf
 
 log "start"
 
+bash $MC_BIN_ATD regist
 mkdir -p $MC_DIR_TMP
 echo -n > ${MC_DIR_LOG}/usb-disk.log
 for f in $(find $MC_DIR_RECORDING $MC_DIR_RECORD_FINISHED $MC_DIR_ENCODING $MC_DIR_PLAY -type f -name '*.xml');do
@@ -13,6 +14,6 @@ $MC_BIN_USB_CONTROL -w
 $MC_BIN_USB_POWER_OFF >> ${MC_DIR_LOG}/usb-disk.log 2>&1
 sudo /etc/init.d/sixad restart
 /bin/rm $MC_STAT_POWEROFF
-bash $($MC_BIN_REALPATH /home/mc/work/invoke.sh)
+bash $($MC_BIN_REALPATH /home/mc/work/invoke.sh) &
 
 log "end"
