d50d94bb2471a232d8524e3424155d922fbffc92
mc <mc@MediaCenter>
2015-02-07 13:24:49 +0900

    play-station-controller

--- xdg-user-dirs/media/bin/play-station-controller.sh
+++ xdg-user-dirs/media/bin/play-station-controller.sh
@@ -15,27 +15,31 @@ if [ "$event_handle" = "not_yet" ];then
     if [ "$type" = "pairing" ];then
 
         sixpair >> $log_file 2>&1
-
         date +"%Y/%m/%d %H:%M:%S.%N pairing finished" >> $log_file
+
         su mc -c \
-        "zenity --info --timeout=10 --display=:0.0 --text=\"<span font_desc='40'>Play Station 3 Controller Pairing finished</span>\"" \
-                >> $log_file 2>&1 &
+        "zenity --info --timeout=10 --display=:0.0 --text=\"<span font_desc='40'>Play Station 3 Controller Pairing finished</span>\"" &
 
     elif [ "$type" = "battery" ];then
 
-        battery_level=$(tac /var/log/sixad | grep -o 'Battery ..' | head -n 1 | awk '{ printf("%d\n", $2) }')
+        (
+            sleep 2
+            battery_level=$(tac /tmp/sixad-task | grep -o 'Battery ..' | head -n 1 | awk '{ printf("%d\n", $2) }')
+            date +"%Y/%m/%d %H:%M:%S.%N battery level $battery_level" >> $log_file
 
-        if [ $battery_level -lt 3 ];then
-            su mc -c \
-            "zenity --info --timeout=20 --display=:0.0 --text=\"<span font_desc='40'>Battery Level is low</span>\"" \
-                    >> $log_file 2>&1 &
-        fi
+            if [ -z "$battery_level" -o "$battery_level" -eq 0 ];then
+                su mc -c \
+                "zenity --info --timeout=20 --display=:0.0 --text=\"<span font_desc='40'>Battery Level is unknown</span>\""
+            elif [ "$battery_level" -lt 3 ];then
+                su mc -c \
+                "zenity --info --timeout=20 --display=:0.0 --text=\"<span font_desc='40'>Battery Level is low</span>\""
+            fi
+        ) &
 
-        date +"%Y/%m/%d %H:%M:%S.%N battery level $battery_level" >> $log_file
     fi
 
 else
-    date +"%Y/%m/%d %H:%M:%S.%N event hendler already finished" >> $log_file
+    date +"%Y/%m/%d %H:%M:%S.%N event handler already finished" >> $log_file
 fi
 
 date +"%Y/%m/%d %H:%M:%S.%N $type END" >> $log_file
