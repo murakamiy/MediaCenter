08ed4440bd21146955c4f5726acd0531ecd112b1
mc <mc@MediaCenter>
2015-07-14 05:57:09 +0900

    player

--- xdg-user-dirs/media/bin/nautilus/main.sh
+++ xdg-user-dirs/media/bin/nautilus/main.sh
@@ -15,28 +15,33 @@ if [ -n "$thumb_file" ];then
 
     base=$(echo $thumb_file | awk -F . '{ print $1 }')
     xml_file=${base}.xml
+    play_stat=false
 
     echo $png_file | grep -q $MC_DIR_TITLE_TS
     if [ $? -eq 0 ];then
-        dir="$MC_DIR_TS_HD $MC_DIR_TS $MC_DIR_MP4_HD $MC_DIR_MP4"
+        play_stat=true
+        for dir in "$MC_DIR_TS_HD $MC_DIR_TS $MC_DIR_MP4_HD $MC_DIR_MP4";do
+            video_file=$(find $dir -type f -name $thumb_file | sort | head -n 1)
+            if [ -n "$video_file" ];then
+                break
+            fi
+        done
     else
         echo $png_file | grep -q $MC_DIR_TITLE_ENCODE
         if [ $? -eq 0 ];then
-            dir=$MC_DIR_ENCODE_HD
+            video_file=$(find $MC_DIR_ENCODE_HD -type f -name $thumb_file | sort | head -n 1)
         else
-            zenity --info --display=:0.0 --text="<span font_desc='40'>something wrong 1\n\n $dir/$thumb_file</span>"
+            zenity --info --display=:0.0 --text="<span font_desc='40'>something wrong 1\n\n $png_file</span>"
             exit
         fi
     fi
 
-    num=$(find $dir -type f -name $thumb_file | wc -l)
-    if [ $num -ne 1 ];then
-        zenity --info --display=:0.0 --text="<span font_desc='40'>something wrong 2\n\n $dir/$thumb_file</span>"
+    if [ -z "$video_file" ];then
+        zenity --info --display=:0.0 --text="<span font_desc='40'>something wrong 2\n\n $png_file</span>"
         exit
     fi
-    video_file=$(find $dir -type f -name $thumb_file)
 
-    if [ "$dir" = "$MC_DIR_TS_HD $MC_DIR_TS $MC_DIR_MP4_HD $MC_DIR_MP4" ];then
+    if [ "$play_stat" = "true" ];then
         trap safe_finish 1 2 3 15
         python ${MC_DIR_DB_RATING}/play.py ${MC_DIR_JOB_FINISHED}/${xml_file} >> ${MC_DIR_DB_RATING}/log 2>&1 &
         pid_play=$!
