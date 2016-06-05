4672c1b22f4837682b39971c9a1bb134e1ab61de
mc <mc@MediaCenter>
2016-06-05 09:47:31 +0900

    do_job

--- xdg-user-dirs/media/bin/do_job.sh
+++ xdg-user-dirs/media/bin/do_job.sh
@@ -54,36 +54,33 @@ else
 
         fifo_dir=/tmp/pt3/fifo
         mkdir -p $fifo_dir
-        fifo_recpt1=${fifo_dir}/recpt1_$$
         fifo_ffmpeg=${fifo_dir}/ffmpeg_$$
         fifo_tail=${fifo_dir}/tee_$$
-        mkfifo -m 644 $fifo_recpt1
         mkfifo -m 644 $fifo_ffmpeg
         mkfifo -m 644 $fifo_tail
 
         if [ "$original_file" = "keep" ];then
-            gst_input=$fifo_tail
-            ffmpeg_output=${MC_DIR_TS}/${job_file_ts}
             job_file_path=${MC_DIR_TS}/${job_file_ts}
         else
-            gst_input=$fifo_ffmpeg
-            ffmpeg_output=$fifo_ffmpeg
             job_file_path=${MC_DIR_MP4}/${job_file_mkv}
         fi
 
         nice -n 5 \
         gst-launch-1.0 -q \
-         filesrc location=$gst_input ! tsdemux name=demux \
+         filesrc location=$fifo_ffmpeg ! tsdemux name=demux \
          demux. ! queue \
                 ! mpegvideoparse \
                 ! vaapidecode \
                 ! queue \
                   leaky=upstream \
+                  max-size-buffers=0 \
+                  max-size-time=0 \
+                  max-size-bytes=500000000 \
                 ! vaapipostproc \
                   deinterlace-mode=auto \
                   deinterlace-method=bob \
                   height=$encode_height \
-                ! vaapiencode_h264 \
+                ! vaapih264enc \
                    tune=high-compression \
                    rate-control=cqp \
                    init-qp=34 \
@@ -93,28 +90,26 @@ else
                   leaky=upstream \
                   max-size-buffers=0 \
                   max-size-time=0 \
-                  max-size-bytes=104857600 \
+                  max-size-bytes=50000000 \
                 ! aacparse \
                 ! mux. \
          matroskamux name=mux ! filesink location=${MC_DIR_MP4}/${job_file_mkv} &
         pid_gst=$!
 
-        ffmpeg -y -i $fifo_recpt1 \
+        ffmpeg -y -i $fifo_tail \
         -loglevel quiet \
         -threads 1 \
         -f mpegts \
         -vcodec copy \
         -acodec libfdk_aac -b:a 256k \
-        $ffmpeg_output &
+        $fifo_ffmpeg &
         pid_ffmpeg=$!
 
-        if [ "$original_file" = "keep" ];then
-            touch ${MC_DIR_TS}/${job_file_ts}
-            tail --follow --retry --sleep-interval=0.5 ${MC_DIR_TS}/${job_file_ts} > $fifo_tail &
-            pid_tail=$!
-        fi
+        touch ${MC_DIR_TS}/${job_file_ts}
+        tail --follow --retry --sleep-interval=0.5 ${MC_DIR_TS}/${job_file_ts} > $fifo_tail &
+        pid_tail=$!
 
-        $MC_BIN_REC --b25 --sid ${ch_array[0]} $rec_channel $rec_time_adjust $fifo_recpt1 &
+        $MC_BIN_REC --b25 --sid ${ch_array[0]} $rec_channel $rec_time_adjust ${MC_DIR_TS}/${job_file_ts} &
         pid_recpt1=$!
         (
             sleep $rec_time_adjust
@@ -132,12 +127,10 @@ else
         (
             sleep 10
 
-            if [ "$original_file" = "keep" ];then
                 kill -TERM $pid_tail
                 sleep 1
                 kill -KILL $pid_tail
                 sleep 5
-            fi
 
             kill -TERM $pid_ffmpeg
             sleep 1
@@ -151,7 +144,6 @@ else
 
         wait $pid_gst
 
-        rm -f $fifo_recpt1
         rm -f $fifo_ffmpeg
         rm -f $fifo_tail
 
@@ -159,6 +151,7 @@ else
             thumb_file=${MC_DIR_THUMB}/${job_file_ts}
         else
             thumb_file=${MC_DIR_THUMB}/${job_file_mkv}
+            rm -f ${MC_DIR_TS}/${job_file_ts}
         fi
         ffmpeg -y -i $job_file_path -f image2 -pix_fmt yuv420p -vframes 1 -ss 5 -s 320x180 -an -deinterlace ${thumb_file}.png > /dev/null 2>&1
         if [ $? -eq 0 ];then
