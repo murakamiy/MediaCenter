f08b86bfe44e6b13abfa0348a190dd6066b32179
mc <mc@MediaCenter>
2016-11-23 20:30:38 +0900

    remove mail log

--- xdg-user-dirs/media/bin/smb_func.sh
+++ xdg-user-dirs/media/bin/smb_func.sh
@@ -213,7 +213,6 @@ function smb_delete_old_file() {
 function smb_put_log() {
 
     count_mc=0
-    count_mail=0
     for ff in $(smb_print_file log | sort -k 1 -nr | awk -F '\t' '{ print $2 }');do
 
         f=$(basename $ff)
@@ -226,14 +225,6 @@ function smb_put_log() {
             fi
         fi
 
-        grep -q ^mail_ <<< $f
-        if [ $? -eq 0 ];then
-            ((count_mail++))
-            if [ $count_mail -gt 2 ];then
-                smbclient -A ~/.smbauth -D log -c "del $f" $MC_SMB_SERVER
-            fi
-        fi
-
     done
 
     yesterday=$(date --date=yesterday +%Y%m%d)
@@ -241,33 +232,6 @@ function smb_put_log() {
     (
         cd $MC_DIR_LOG
         smbclient -A ~/.smbauth -D log -c "put $yesterday mc_${yesterday}.txt" $MC_SMB_SERVER
-    )
-
-    mail=mail_${yesterday}.txt
-    LC_ALL=C awk '
-    BEGIN {
-        begin = 0
-        end = 0
-        yesterday = strftime("^From .+ %a %b %d [0-9]{2}:[0-9]{2}:[0-9]{2} %Y$", systime() - 60 * 60 * 24)
-        today     = strftime("^From .+ %a %b %d [0-9]{2}:[0-9]{2}:[0-9]{2} %Y$", systime())
-    }
-
-    {
-        if (begin == 0 && match($0, yesterday)) {
-            begin = 1
-        }
-        else if (begin == 1 && end == 0 && match($0, today)) {
-            end = 1
-        }
-        if (begin == 1 && end == 0) {
-            print $0
-        }
-    }
-    ' /var/mail/mc > /tmp/$mail
-
-    (
-        cd /tmp
-        smbclient -A ~/.smbauth -D log -c "put $mail" $MC_SMB_SERVER
     )
 }
 
