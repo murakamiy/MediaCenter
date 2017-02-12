d8b353fd5553e8c5df25e6f87f927e3ae161f467
mc <mc@MediaCenter>
2017-02-12 12:52:16 +0900

    startup.sh

--- xdg-user-dirs/media/bin/startup.sh
+++ xdg-user-dirs/media/bin/startup.sh
@@ -7,11 +7,11 @@ mkdir -p $MC_DIR_TMP
 mkdir -p $MC_DIR_FIFO
 for f in $(find $MC_DIR_RECORDING $MC_DIR_RECORD_FINISHED $MC_DIR_ENCODING $MC_DIR_ENCODING_CPU $MC_DIR_ENCODING_GPU $MC_DIR_PLAY -type f -name '*.xml');do
     log "failed: $f"
-    /bin/mv $f $MC_DIR_FAILED
+    mv -f $f $MC_DIR_FAILED
 done
 
-/bin/rm $MC_STAT_POWEROFF
-/bin/rm $MC_ABORT_SHUTDOWN
+rm -f $MC_STAT_POWEROFF
+rm -f $MC_ABORT_SHUTDOWN
 
 $MC_BIN_DISK_CONTROL -o
 
