4d7b8b7945d5c02af4411579a96429d4d40de082
mc <mc@MediaCenter>
2017-04-10 19:08:42 +0900

    playstation controller 4 battery

--- xdg-user-dirs/media/bin/login.sh
+++ xdg-user-dirs/media/bin/login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $(dirname $0)/00.conf
 
-battery_level=$(upower -i /org/freedesktop/UPower/devices/keyboard_sony_controller_battery_04o98of3o72oddo2c | grep percentage | awk '{ printf("%d", $2) }')
+battery_level=$(upower -i $(upower -e | grep sony_controller_battery) | grep percentage | awk '{ printf("%d", $2) }')
 if [ "$battery_level" -ge 0 -a "$battery_level" -le 100 ];then
     sleep_time=$((60 * 30 * ($battery_level / 25)))
 else
