6d02ef047170ede76555f656b007ce0e5348efea
mc <mc@MediaCenter>
2017-04-22 14:34:20 +0900

    ffmpeg

--- xdg-user-dirs/media/files/EncodeServer/encode/bin/gpu_encode.sh
+++ xdg-user-dirs/media/files/EncodeServer/encode/bin/gpu_encode.sh
@@ -29,16 +29,14 @@ ffmpeg -y $seek_param \
 -loglevel warning \
 -analyzeduration 30M \
 -probesize 100M \
--hwaccel cuvid \
--c:v mpeg2_cuvid \
 -i async:tcp://${ip_addr_recive}:${EN_PORT_NO_GPU_RECIEVE}?listen \
--vf "scale_npp=w=${encode_width}:h=${encode_height}:interp_algo=super" \
+-vf "scale=w=${encode_width}:h=${encode_height}" \
 -preset:v 5 \
 -profile:v 0 \
 -level 30 \
 -rc 0 \
 -cq 28 \
--global_quality 28 \
+-qp 28 \
 -c:v h264_nvenc \
 $volume_adjust_param -c:a aac \
 -f matroska \
